b5c8d3cc603ba256d2e74bd04bed741641dea986
saintyoga-Cyber <saintyoga@gmail.com>
2026-01-21 20:29:38 -0800

    Update: SonicFTW watchapp with resources

---
+++ src/c/main.c
@@ -0,0 +1,33 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_text_layer;
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  s_text_layer = text_layer_create(GRect(0, bounds.size.h/2 - 20, bounds.size.w, 40));
+  text_layer_set_text(s_text_layer, "Sonic Control");
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_text_layer);
+}
+
+int main(void) {
+  s_main_window = window_create();
+  
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  
+  window_stack_push(s_main_window, true);
+  app_event_loop();
+  window_destroy(s_main_window);
+}
